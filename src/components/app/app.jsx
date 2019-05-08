6320bab77fce692e21055585f1c48e2cb0998a14
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2019-05-09 01:31:59 +0200

    Add app component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
   return <div>
-    <div style={display: none}>
+    <div style={{display: `none`}}>
       <svg xmlns="http://www.w3.org/2000/svg"><symbol id="icon-arrow-select" viewBox="0 0 7 4"><path fillRule="evenodd" clipRule="evenodd" d="M0 0l3.5 2.813L7 0v1.084L3.5 4 0 1.084V0z"></path></symbol><symbol id="icon-bookmark" viewBox="0 0 17 18"><path d="M3.993 2.185l.017-.092V2c0-.554.449-1 .99-1h10c.522 0 .957.41.997.923l-2.736 14.59-4.814-2.407-.39-.195-.408.153L1.31 16.44 3.993 2.185z"></path></symbol><symbol id="icon-star" viewBox="0 0 13 12"><path fillRule="evenodd" clipRule="evenodd" d="M6.5 9.644L10.517 12 9.451 7.56 13 4.573l-4.674-.386L6.5 0 4.673 4.187 0 4.573 3.549 7.56 2.483 12 6.5 9.644z"></path></symbol></svg>
     </div>
 
@@ -67,7 +67,7 @@ const App = () => {
           </ul>
         </section>
       </div>
-      {/* <div className="cities__places-wrapper">
+      <div className="cities__places-wrapper">
         <div className="cities__places-container container">
           <section className="cities__places places">
             <h2 className="visually-hidden">Places</h2>
@@ -112,7 +112,7 @@ const App = () => {
                   </div>
                   <div className="place-card__rating rating">
                     <div className="place-card__stars rating__stars">
-                      <span style="width: 93%"></span>
+                      <span style={{width: `93%`}}></span>
                       <span className="visually-hidden">Rating</span>
                     </div>
                   </div>
@@ -144,7 +144,7 @@ const App = () => {
                   </div>
                   <div className="place-card__rating rating">
                     <div className="place-card__stars rating__stars">
-                      <span style="width: 80%"></span>
+                      <span style={{width: `80%`}}></span>
                       <span className="visually-hidden">Rating</span>
                     </div>
                   </div>
@@ -176,7 +176,7 @@ const App = () => {
                   </div>
                   <div className="place-card__rating rating">
                     <div className="place-card__stars rating__stars">
-                      <span style="width: 80%"></span>
+                      <span style={{width: `80%`}}></span>
                       <span className="visually-hidden">Rating</span>
                     </div>
                   </div>
@@ -211,7 +211,7 @@ const App = () => {
                   </div>
                   <div className="place-card__rating rating">
                     <div className="place-card__stars rating__stars">
-                      <span style="width: 100%"></span>
+                      <span style={{width: `100%`}}></span>
                       <span className="visually-hidden">Rating</span>
                     </div>
                   </div>
@@ -243,7 +243,7 @@ const App = () => {
                   </div>
                   <div className="place-card__rating rating">
                     <div className="place-card__stars rating__stars">
-                      <span style="width: 80%"></span>
+                      <span style={{width: `80%`}}></span>
                       <span className="visually-hidden">Rating</span>
                     </div>
                   </div>
@@ -259,7 +259,7 @@ const App = () => {
             <section className="cities__map map"></section>
           </div>
         </div>
-      </div> */}
+      </div>
     </main>
   </div>;
 };
