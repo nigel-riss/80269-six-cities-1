032cc3308461b0b5745be742071dd789c7b957d0
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2019-05-15 21:00:53 +0200

    Add PlaceCard component and PropTypes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app.jsx';
 
+const placesNames = [
+  `Beautiful &amp; luxurious apartment at great location`,
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`,
+];
+
 const init = () => {
-  ReactDOM.render(<App />, document.querySelector(`#root`));
+  ReactDOM.render(
+      <App
+        places = {placesNames}
+      />,
+      document.querySelector(`#root`)
+  );
 };
 
 init();
